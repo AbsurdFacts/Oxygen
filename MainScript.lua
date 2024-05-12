eac90fb5c2541fab74843e60b54a9ae162e80179
AbsurdFacts <163673134+AbsurdFacts@users.noreply.github.com>
2024-05-11 20:02:43 -0400

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -2,6 +2,19 @@
 so yea oxygen 1.8 w
 ]]--
 
+local file = io.open("oxygen.txt", "w")
+
+if file then
+    -- Write content to the file
+    file:write("lol")
+    file:write("this aint done yet")
+
+    -- Close the file
+    file:close()
+else
+    print("Failed to open the file for writing.")
+end
+
 local MainGui = Instance.new("ScreenGui")
 local OpenFrame = Instance.new("TextButton")
 local UICorner = Instance.new("UICorner")
@@ -614,17 +627,3 @@ local function BBFYKBH_fake_script() -- MainGui.LocalScript
 	end)
 end
 coroutine.wrap(BBFYKBH_fake_script)()
-
--- Open a file in write mode (w) or create it if it doesn't exist
-local file = io.open("oxygen.txt", "w")
-
-if file then
-    -- Write content to the file
-    file:write("lol")
-    file:write("this aint done yet")
-
-    -- Close the file
-    file:close()
-else
-    print("Failed to open the file for writing.")
-end
